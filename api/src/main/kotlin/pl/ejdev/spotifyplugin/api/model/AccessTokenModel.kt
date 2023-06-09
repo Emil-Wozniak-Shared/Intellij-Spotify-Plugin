bfe96fa64cd1d51281c2724312a8cdf81ec3e16f
ewozniak <emil.wozniak.591986@gmail.com>
2023-06-09 19:49:35 +0200

    feat: http client

---
+++ api/src/main/kotlin/pl/ejdev/spotifyplugin/api/model/AccessTokenModel.kt
@@ -0,0 +1,7 @@
+package pl.ejdev.spotifyplugin.api.model
+
+data class AccessTok1enModel(
+    val access_token: String,
+    val token_type: String,
+    val expires_in: Int
+)
--- api/src/test/kotlin/pl/ejdev/spotifyplugin/api/service/SpotifyAccessTokenServiceTest.kt
+++ api/src/test/kotlin/pl/ejdev/spotifyplugin/api/service/SpotifyAccessTokenServiceTest.kt
@@ -1,17 +1,28 @@
 package pl.ejdev.spotifyplugin.api.service
 
 import io.kotest.core.spec.style.FeatureSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldNotBeEmpty
+import java.net.http.HttpClient
 
 class SpotifyAccessTokenServiceTest : FeatureSpec({
     feature("Spotify access token service ") {
         val subject = SpotifyAccessTokenService()
 
         scenario("returns access token from Spotify API") {
-            val tokenService = subject.requestAccessToken()
-            val accessToken = subject.getAccessToken()
+            val response = subject.requestToken()
 
-            accessToken.shouldNotBeEmpty()
+            response.statusCode() shouldBe 200
+            response.uri().toString() shouldBe "https://accounts.spotify.com/api/token"
+            with(response.body()) {
+                access_token.shouldNotBeEmpty()
+                token_type shouldBe "Bearer"
+                expires_in shouldBe 3600
+            }
+            with(response.request()) {
+                method() shouldBe "POST"
+                headers().map() shouldBe mapOf("content-type" to listOf("application/x-www-form-urlencoded"))
+            }
         }
     }
 })
