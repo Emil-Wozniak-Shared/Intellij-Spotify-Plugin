409b37c37a9ad55bb3513c56502031b05bf31193
Emil Woźniak <81687494+Emil-Wozniak-Shared@users.noreply.github.com>
2023-06-10 13:33:55 +0200

    fix: setup logger (#7)
    Co-authored-by: ewozniak <emil.wozniak.591986@gmail.com>

--- api/src/main/kotlin/pl/ejdev/spotifyplugin/api/service/SpotifyAccessTokenService.kt
+++ api/src/main/kotlin/pl/ejdev/spotifyplugin/api/service/SpotifyAccessTokenService.kt
@@ -1,6 +1,7 @@
 package pl.ejdev.spotifyplugin.api.service
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import mu.KotlinLogging
 import pl.ejdev.spotifyplugin.api.configuration.*
 import pl.ejdev.spotifyplugin.api.model.AccessTok1enModel
 import java.net.URI
@@ -11,12 +12,14 @@ import java.net.http.HttpResponse.BodyHandler
 import java.net.http.HttpResponse.BodySubscribers
 import java.net.http.HttpResponse.BodySubscribers.ofInputStream
 
-
 private const val PAYLOAD = "grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}"
 
 object SpotifyAccessTokenService {
+    private val logger = KotlinLogging.logger { }
+
     fun requestToken(): HttpResponse<AccessTok1enModel> =
-        httpClient.send(tokenRequest, bodyHandler)
+        logger.warn ( "Request token" )
+            .run { httpClient.send(tokenRequest, bodyHandler) }
 
     private val bodyHandler: BodyHandler<AccessTok1enModel> = BodyHandler<AccessTok1enModel> {
         BodySubscribers.mapping(ofInputStream(), objectMapper::readValue)
